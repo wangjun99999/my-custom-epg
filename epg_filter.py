8d40e5e675b8967bce4f4bfc69686aca1d289bb2
wangjun99999 <45650872@qq.com>
2025-12-15 17:27:03 +0800

    Update epg_filter.py

--- epg_filter.py
+++ epg_filter.py
@@ -10,7 +10,7 @@ import time
 CHANNEL_TXT_FILE = "channel_list.txt"
 RAW_EPG_URLS = [
     "https://epg.pw/xmltv/epg.xml",
-    "https://raw.githubusercontent.com/zzq1234567890/epg/refs/heads/main/epgnew.xml",
+    "https://raw.githubusercontent.com/zzq1234567890/epg/refs/heads/main/epgziyong.xml",
     "https://raw.githubusercontent.com/myhomebox/EPG/refs/heads/main/output/4g.xml",
     "https://raw.githubusercontent.com/myhomebox/EPG/refs/heads/main/output/hami.xml",
     "https://raw.githubusercontent.com/nightah/daddylive/refs/heads/main/epgs/daddylive-channels-epg.xml",
